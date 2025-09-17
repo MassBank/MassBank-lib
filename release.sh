c949becdf981922ed4aa5dc2cd065e1c88d595de
René Meier <meier.rene@googlemail.com>
2025-09-17 14:09:52 +0200

    Add citation infrastructure

--- release.sh
+++ release.sh
@@ -9,13 +9,18 @@ fi
 RELEASE_VERSION="$1"
 NEXT_SNAPSHOT_VERSION="$2"
 
-# 1. Auf dev: Release-Version setzen und committen
+# 1. set version in dev and commit
 git checkout dev
 mvn versions:set -DnewVersion="$RELEASE_VERSION"
 mvn versions:commit
 git add pom.xml
+# set version in README.md
 sed -i "s/\(<version>\)[0-9]\+\.[0-9]\+\.[0-9]\+\(-SNAPSHOT\)\?\(<\/version>\)/\1$RELEASE_VERSION\3/" README.md
 git add README.md
+#set version and date in CITATION.cff
+sed -i "s/^version: .*/version: \"$RELEASE_VERSION\"/" CITATION.cff
+sed -i "s/^date-released: .*/date-released: \"$(date +%Y-%m-%d)\"/" CITATION.cff
+git add CITATION.cff
 git commit -m "Release version $RELEASE_VERSION"
 git push origin dev
 
